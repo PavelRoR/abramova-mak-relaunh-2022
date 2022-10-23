24190b37d917ff63b01d931b3d8edc349f57d5fd
PavelRoR <pavelvp2182@yandex.ru>
2022-10-23 17:19:27 +0500

    Main page typeset is done

--- _app/_js/scripts.js
+++ _app/_js/scripts.js
@@ -1,5 +1,113 @@
 $(document).ready(function() {
+    $(function () {
+        var check = $('.check', this),
+            email = $('.send-input', this),
+            message = $('.alert-message', this),
+            button = $('.send-button', this);
+        $(".send-form").on("submit", function () {
+            var check = $('.check', this),
+                message = $('.alert-message', this),
+                reNone = /.+/,
+                email = $('.send-input', this),
+                button = $('.send-button', this);
+            if (!email.val().match(reNone)) {
+                message.text('Введите email').slideDown(500);
+                return false;
+            }
+            if (!check.prop("checked")) {
+                message.text('Подтвердите соглашение').slideDown(500);
+                return false;
+            }
+            if (email.val() && check) {
 
+                // button.text('Отправляем...');
+                // setTimeout(function () {
+                //     button.text('Отправлено!');
+                // }, 500);
+                return true
+            }
+        });
+        email.click(function () {
+            // email.css({"borderColor": "rgb(25, 10, 12)",'transition':'all .4s ease'});
+            message.slideUp(500);
+        });
+        check.click(function () {
+            message.slideUp(500);
+        });
+    });
+    $('.button-up').fancybox();
+    $('[data-fancybox]').fancybox({
+        loop: 1,
+    });
+    $('.revs-slider-video').slick({
+        autoplay: false,
+        autoplaySpeed: 3000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        speed: 300,
+        arrows: true,
+        centerPadding: '40px',
+        // adaptiveHeight: true,
+        centerMode: true,
+        // appendArrows: '.video-revs-arrows-1',
+        responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1
+            }
+        }
+        ]
+    });
+    $('.revs-slider-text').slick({
+        autoplay: false,
+        autoplaySpeed: 3000,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        speed: 300,
+        arrows: true,
+        centerPadding: '40px',
+        // adaptiveHeight: true,
+        centerMode: true,
+        // appendArrows: '.video-revs-arrows-2',
+        responsive: [
+        {
+            breakpoint: 561,
+            settings: {
+                slidesToShow: 1
+            }
+        }
+        ]
+    });
+    $('.slick-dots').wrap('<div class="container container-revs">')
+    /* Видео */
+    $(".video-wrapper-revs img").click(function () {
+        var a = $(this).parent().attr("data-youtube");
+        $(this).parent().html('<iframe src="https://www.youtube.com/embed/' + a + '?showinfo=0&rel=0&autoplay=1&modestbranding=1&mute=1&showinfo=0&rel=0" allowfullscreen></iframe>')
+    });
+    function imToVideo(){
+        $('.video-wrapper-revs iframe').each(function () {
+            var l = $(this).parent().attr('data-img');
+            $(this).parent().html('<img src="' + l + '" alt="Видео отзыв">');
+        });
+        $(".video-wrapper-revs img").click(function () {
+            var a = $(this).parent().attr("data-youtube");
+            $(this).parent().html('<iframe src="https://www.youtube.com/embed/' + a + '?modestbrandig=1&mute=1&showinfo=0&rel=0&autoplay=1" allowfullscreen></iframe>');
+        });
+    }
+$('.revs-slider').on('swipe', function (event, slick, direction) {
+    imToVideo();
+});
+$('.revs-slider').on('afterChange', function (event, slick, direction) {
+    imToVideo();
+});
+$('.revs-slider').on('beforeChange', function (event, slick, direction) {
+    imToVideo();
+});
 
 
 /*Конец документа*/
